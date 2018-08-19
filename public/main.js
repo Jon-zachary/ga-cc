7f041d790755df57494b53efef4b883b47a7e69f
Jon Zachary <jontzachary@gmail.com>
2018-08-18 20:21:40 -0400

    showAll working, getOne working.

--- public/main.js
+++ public/main.js
@@ -1,22 +1,48 @@
+/* eslint-env browser */
+
 console.log('main.js online');
 
 const submitButton = document.querySelector('#search-button');
 const movieSearch = document.querySelector('#movie-search');
-const data = [];
+const movieList = document.querySelector('.movieList');
+const baseUrl = 'http://www.omdbapi.com';
+const apiKey = 'c9d5cf65';
+let data = [];
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const query = movieSearch.value;
-  const baseUrl = 'http://www.omdbapi.com';
-  const apiKey = 'c9d5cf65';
-  const init = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default',
-  };
-  fetch(`${baseUrl}/?s=${query}&page=1&apikey=${apiKey}`, init)
-    .then(res => res.json())
-    .then(json => console.log(json));
+const getSingleMovie = (id) => {
+  movieList.innerHTML = '';
+  const query = id;
+  const movie = fetch(`${baseUrl}/?i=${query}&apikey=${apiKey}`)
+    .then(res => res.json());
+  return movie;
 };
 
-submitButton.addEventListener('click', handleSubmit);
+const showSingleMovie = (e) => {
+  getSingleMovie(e.target.dataset.id)
+    .then((movie) => {
+      console.log(movie);
+    });
+};
+
+const showMovies = (moviesPromise) => {
+  moviesPromise.then((movies) => {
+    movies.Search.forEach((movie) => {
+      const movieP = document.createElement('p');
+      movieP.textContent = movie.Title;
+      movieP.dataset.id = movie.imdbID;
+      movieP.addEventListener('click', showSingleMovie);
+      movieList.appendChild(movieP);
+      console.log(movie);
+    });
+  });
+};
+
+const getAllMovies = (e) => {
+  e.preventDefault();
+  const query = movieSearch.value;
+  data = fetch(`${baseUrl}/?s=${query}&page=1&apikey=${apiKey}`)
+    .then(res => res.json());
+  showMovies(data);
+};
+
+submitButton.addEventListener('click', getAllMovies);
