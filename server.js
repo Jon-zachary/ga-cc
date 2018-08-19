5048606a14e8825cd8f62180f1a7647621516ad6
Jon Zachary <jontzachary@gmail.com>
2018-08-19 16:05:10 -0400

    clean up server.js

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var path = require('path');
-var bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
+app.get('/favorites', (req, res) => {
+  const data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
-app.post('/favorites', function(req, res){
-  if(!req.body.Title || !req.body.imdbID){
-    res.send("Error");
+app.post('/favorites', (req, res) => {
+  if (!req.body.Title || !req.body.imdbID) {
+    res.send('Error');
   }
 
-  var data = JSON.parse(fs.readFileSync('./data.json'));
+  const data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
